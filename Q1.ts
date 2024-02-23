c8b48435109a2120cc9e7ef6323cee2de6436a2b
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-02-23 03:25:02 -0800

    Q-01 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-01

---
+++ Q1.ts
@@ -0,0 +1,13 @@
+// 1.	Install Node.js, TypeScript and VS Code on your computer
+
+// to download node.js
+// Nodejs.org/en/dowload 
+// node -v (to chek version on cmd)
+
+// install typescript
+//  npm i -g typescript (installation on cmd)
+// tsc -v (to chek version on cmd)
+
+// install vs code
+// download visual studio code
+// code .    (to chek vs code) 
